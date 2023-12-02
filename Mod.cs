8f9eaec8f91400330744f53191bafeffb6651015
18107 <18107@users.noreply.github.com>
2023-12-02 11:14:24 +1030

    Add files via upload

---
+++ Mod.cs
@@ -0,0 +1,22 @@
+﻿using PulsarModLoader;
+
+namespace Scrap_Auto_Process
+{
+    public class Mod : PulsarMod
+    {
+        public override string Version => "0.0.0";
+
+        public override string Author => "18107";
+
+        public override string ShortDescription => "Automatically process scrap as soon as it is collected or purchased";
+
+        public override string Name => "Scrap Auto Process";
+
+        public override string ModID => "scrapautoprocess";
+
+        public override string HarmonyIdentifier()
+        {
+            return "id107.scrapautoprocess";
+        }
+    }
+}
---
+++ ShipStats.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+
+namespace Scrap_Auto_Process
+{
+    [HarmonyPatch(typeof(PLShipStats), "AddShipComponent")]
+    internal class ShipStats
+    {
+        static void Postfix(PLShipStats __instance, PLShipComponent inComponent)
+        {
+            if (__instance.Ship != PLEncounterManager.Instance.PlayerShip || !(inComponent is PLScrapCargo) || !PhotonNetwork.isMasterClient) return;
+
+            PLServer.Instance.GetCachedFriendlyPlayerOfClass(0).photonView.RPC("AttemptToProcessScrapCargo", PhotonTargets.MasterClient, new object[]
+                    {
+                        __instance.Ship.ShipID,
+                        inComponent.NetID
+                    });
+        }
+    }
+}
